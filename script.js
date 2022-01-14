e1aa2360a1a86b3e411225faaa129f551524e354
fariasmi <mi.farias182@gmail.com>
2022-01-14 08:40:57 -0300

    library project start

---
+++ script.js
@@ -0,0 +1,41 @@
+let myLibrary = [];
+
+function Book (){
+    this.title = title;
+    this.author = author;
+    this.numberPages = numberPages;
+    this.isRead = isRead;        
+}
+
+
+function addBookToLibrary(bookObj){
+    myLibrary.push(bookObj);
+
+}
+
+function displayBooks(){
+    console.log(myLibrary);
+}
+
+let book1 = {
+    "title": "The Lord of the rings : Two towers",
+    "author": "R.R.Tolkien",
+    "numberPages": 352,
+    "isRead": true
+   };
+let book2 = {
+    "title": "The Lord of the rings :return of the king ",
+    "author": "R.R.Tolkien",
+    "numberPages": 431,
+    "isRead": true
+   };
+
+
+book1.prototype = Object.create(Book.prototype);
+book2.prototype = Object.create(Book.prototype);
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+
+displayBooks();
+
