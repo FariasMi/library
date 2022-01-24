84806e63a73cdc073202c4b48349752ae80dbbe8
fariasmi <mi.farias182@gmail.com>
2022-01-24 09:45:25 -0300

    'a'

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
 let myLibrary = [];
 
-function Book (){
+function Book (title,author,numberPages,isRead){
     this.title = title;
     this.author = author;
     this.numberPages = numberPages;
     this.isRead = isRead;        
 }
 
+const createBookfromForm =(e) =>{
+     e.preventDefault();
+      let book = {
+        title:document.getElementById('title').value,
+        author:document.getElementById('author').value,
+        numberPages: document.getElementById('pages').value,
+        isRead:document.getElementById('read').checked
+    }
+       
+    book.prototype = Object.create(Book.prototype); 
+    addBookToLibrary(book);
+    document.forms[0].reset();
+    displayBooks();
 
-function addBookToLibrary(bookObj){
+}
+
+const addBookToLibrary = (bookObj)=>{
     myLibrary.push(bookObj);
 
 }
 
-function displayBooks(){
+const displayBooks = ()=>{
     console.log(myLibrary);
+}
+
+const saveLocalStorage =() =>{
+    return;
+}
+
+const validateForm = ()=>{
+   const title = document.forms['form']['title'].value;
+   const author = document.forms['form']['author'].value;
+   const pages = document.forms['form']['pages'].value;
+   // if(title ==''){
+  // /     document.getElementById('title').style.borderColor = 'red';
+   // }
 }
 
 let book1 = {
@@ -22,12 +50,12 @@ let book1 = {
     "author": "R.R.Tolkien",
     "numberPages": 352,
     "isRead": true
-   };
+}
 let book2 = {
     "title": "The Lord of the rings :return of the king ",
     "author": "R.R.Tolkien",
     "numberPages": 431,
-    "isRead": true
+    "isRead": false
    };
 
 
@@ -37,14 +65,30 @@ book2.prototype = Object.create(Book.prototype);
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 
-displayBooks();
+
+   
 
 /*parte do dom */
+
+let formArea = document.getElementsByClassName('form-area')[0];
+let form = document.getElementById('book-form');
+
+
 function openAddBookForm(){
     
-    document.getElementsByClassName('form-area')[0].style.display = 'block';
+    formArea.style.display = 'block';
 
 }
 
+function closeAddBookForm(){
+    formArea.style.display = 'none';
+}
+
+
     document.getElementById('add-book').addEventListener('click',openAddBookForm);
-    
+    document.getElementById('close-form').addEventListener('click',closeAddBookForm);
+    document.getElementById('add-form').addEventListener('click',createBookfromForm);
+
+
+
+   
