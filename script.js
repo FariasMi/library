aa3f23c31c476a6e49d5ac5d1b24824f431149fe
fariasmi <mi.farias182@gmail.com>
2022-02-03 12:29:47 -0300

    validate if the book alread exists

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const getBookfromForm =() =>{
         book.prototype = Object.create(Book.prototype); 
         addBookToLibrary(book);
         document.forms[0].reset();
-        displayBooks();
+        
     }
     
 }
@@ -46,7 +46,7 @@ const displayBooks = (book)=>{
    
 }
 
-const saveLocalStorage =(book)=>{
+const saveLocalStorage =()=>{
         localStorage.setItem('myLibraryList',JSON.stringify(myLibrary));
 
     }
@@ -62,8 +62,8 @@ const createBookGrid = (book) =>{
     const readBtn   = document.createElement('btn');
 
     bookCard.classList.add('book-card');
-    readBtn.classList.add('btn','read-book','btn-action');
-    deleteBtn.classList.add('btn','delete-book','btn-action');
+    readBtn.classList.add('btn','read-book');
+    deleteBtn.classList.add('btn','delete-book');
     
     
     title.textContent = `"${book.title}"`;
@@ -90,39 +90,37 @@ const createBookGrid = (book) =>{
         if(book.isRead){
             readBtn.style.backgroundColor = 'rgba(219,32,63,0.5)';
             readBtn.textContent = 'Not read';
-            book.isRead = false;
+            book.isRead = false;            
         }else{
             readBtn.style.backgroundColor = 'rgba(60, 179, 113,0.5)';
             readBtn.textContent = 'Read';
-            book.isRead = true;
+            book.isRead = true;          
 
         }
+        saveLocalStorage();
     });
 
    deleteBtn.addEventListener('click',()=>{
         deleteBook(book);
         bookCard.remove();
+        saveLocalStorage();
 
    });
 
 }
 
 const verifyIfTheBookExists = (book) =>{
-    let found = {};
-    myLibrary.forEach((bookInLibrary)=>{
-        if(bookInLibrary.name ===book.name){
-            found = bookInLibrary;
-            console.log(found);
-        }
-    });
+   const found_books= myLibrary.filter(objBook => objBook.title === book.title &&  objBook.author === book.author);
+        
+   if(Object.keys(found_books).length !=0 ){
+        alert('Hey this book alread exists');
+        return true;      
+   }
+       return false;
 
-    if(found.title && found.author === book.title && book.author){
-        alert('Hey! This book already exists');
-        return true;
-    }
-      return false;
+   }
 
-}
+
 const getBooksFromLocalStorage = ()=>{
     let jsonFromLocalStorage = localStorage.getItem('myLibraryList');
     if(jsonFromLocalStorage != null){
@@ -134,12 +132,13 @@ const getBooksFromLocalStorage = ()=>{
 
 const displayBooksThatAlreadyExist = ()=>{
     if(myLibrary.length != null){
-        for(i=0;i<myLibrary.length;i++){
-            createBookGrid(myLibrary[i])
-        }
-    }
+        myLibrary.forEach((bookInLibrary)=>{
+            createBookGrid(bookInLibrary);
+        });
+    }   
        
 }
+
 
 const validateForm = (e)=>{
    const title = document.getElementById('title');
@@ -150,17 +149,18 @@ const validateForm = (e)=>{
    if(title.value == '' || title.value == null){
        error = true;
        title.style.borderBottom = ' dotted #ff0000';
-       title.placeholder = "you must to fill the title"
+       title.placeholder = 'You need to fill the title';
+       
    }
    if(author.value == '' || title.value == null){
        error = true;
        author.style.borderBottom = ' dotted #ff0000';
-       author.placeholder = "you must to fill the author"
+       author.placeholder = 'You need to fill the author';
    }
    if(pages.value == '' || pages.value == null){
         error = true;
         pages.style.borderBottom = ' dotted #ff0000';
-        pages.placeholder = "you must to fill the pages"
+        pages.placeholder = 'You need to fill the pages';
    }
 
     if(!error){
